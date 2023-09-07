3f35d329a6f195167ff17c628765972e4a995fd1
yantavares <yantdo1@gmail.com>
2023-09-07 15:24:54 -0300

    added files for first project

--- trab1/Game.cpp
+++ trab1/Game.cpp
@@ -1,8 +1,79 @@
-#include <iostream>
 #include <Game.hpp>
 
-int main(int argc, char const *argv[])
-{
+Game *Game::instance = nullptr;
 
-    return 0;
+Game::Game(const std::string &title, int width, int height)
+{
+    if (instance != nullptr)
+    {
+        std::cerr << "Error: Only one instance of Game is allowed." << std::endl;
+        exit(1);
+    }
+    instance = this;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cerr << "Error: Failed to initialize SDL: " << SDL_GetError() << std::endl;
+        exit(1);
+    }
+
+    if ((IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF) & (IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF)) != (IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF))
+    {
+        std::cerr << "Error: Failed to initialize SDL_Image: " << IMG_GetError() << std::endl;
+        exit(1);
+    }
+
+    if (Mix_Init(MIX_INIT_OGG) < 0)
+    {
+        std::cerr << "Error: Failed to initialize SDL_mixer: " << Mix_GetError() << std::endl;
+        exit(1);
+    }
+
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024) < 0)
+    {
+        std::cerr << "Error: Failed to open audio device: " << Mix_GetError() << std::endl;
+        exit(1);
+    }
+
+    Mix_AllocateChannels(32);
+
+    window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
+
+    if (window == nullptr)
+    {
+        std::cerr << "Error: Failed to create SDL window: " << SDL_GetError() << std::endl;
+        exit(1);
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr)
+    {
+        std::cerr << "Error: Failed to create SDL renderer: " << SDL_GetError() << std::endl;
+        exit(1);
+    }
+}
+
+Game::~Game()
+{
+    delete state;
+
+    Mix_CloseAudio();
+    Mix_Quit();
+
+    IMG_Quit();
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+}
+
+State &Game::GetState()
+{
+    return *state;
+}
+
+SDL_Renderer *Game::GetRenderer()
+{
+    return renderer;
 }
--- trab1/Game.hpp
+++ trab1/Game.hpp
@@ -1,3 +1,24 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_mixer.h>
+#include <iostream>
+
+using namespace std;
+
+class State;
+class Game
+{
+public:
+    Game(const string &title, int width, int height);
+    ~Game();
+    void Run();
+    SDL_Renderer *GetRenderer();
+    State &GetState();
+    static Game &GetInstance();
+
+private:
+    static Game *instance;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    State *state;
+};
