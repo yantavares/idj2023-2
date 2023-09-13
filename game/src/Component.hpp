c13100071e2dccf90f994a41f09bf8371c3381f0
yan <yantdo1@gmail.com>
2023-09-13 14:58:43 -0300

    added all files for project2

---
+++ game/src/Component.hpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class Component
+{
+private:
+    /* data */
+public:
+    Component(/* args */);
+    ~Component();
+};
+
+Component::Component(/* args */)
+{
+}
+
+Component::~Component()
+{
+}
