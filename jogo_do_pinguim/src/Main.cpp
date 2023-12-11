a208887d79b8686041e457eb5e08cba84051a70e
yan <yantdo1@gmail.com>
2023-12-11 19:21:03 -0300

    Add StageState to the game

--- jogo_do_pinguim/src/Main.cpp
+++ jogo_do_pinguim/src/Main.cpp
@@ -1,11 +1,22 @@
 #include <SDL2/SDL.h>
+#include <ctime>
 
 #include "game/Game/Game.hpp"
+#include "../StageState/StageState.hpp" // Inclua o caminho correto para StageState
 
 int main(int argc, char **argv)
 {
     srand(time(NULL));
+
     Game &game = Game::GetInstance();
+    
+    // Cria uma instância de StageState e dá push no jogo
+    StageState* stageState = new StageState();
+    
+    game.Push(stageState);
+
+    // Executa o jogo
     game.Run();
+
     return 0;
 }
